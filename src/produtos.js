7b29b9ebdd50d6a85da3fb9745b4cbba2e3c6f83
Pedro Lima <pedroliveiralima21@gmail.com>
2022-11-29 13:43:33 -0300

    ex de arrays. add arquivo produtos com o exercicios

---
+++ src/produtos.js
@@ -0,0 +1,15 @@
+const produtos = [
+    {id:1, nome: 'detergente', valor: 2.00, categoria: 'limpeza'},
+    {id:2, nome: 'amaciante', valor:6.50, categoria:'limpeza'},
+    {id:3, nome: 'bolacha', valor: 2.00, categoria:'alimento'},
+    {id:4, nome: 'cerveja', valor: 7.00, categoria:'alimento'},
+    {id:5, nome: 'saco de bala', valor: 2.20, categoria:'alimento'}
+]
+
+const ids = produtos.map(produto => produto.id);
+const nomes = produtos.map(produto => produto.nome)
+
+const numeros = [ 1,2,3,4,5]
+
+
+console.log(nomes)
